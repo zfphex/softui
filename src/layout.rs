6efdede0f3fba8069d414059edeaa3a5a4165be4
Bay <28522740+zX3no@users.noreply.github.com>
2024-06-03 15:00:16 +0930

    testing paddding

--- src/layout.rs
+++ src/layout.rs
@@ -3,6 +3,7 @@ use crate::*;
 pub const fn v<T: Tuple>(mut widgets: T) -> Container<T> {
     Container {
         widgets,
+        area: None,
         direction: Direction::Vertical,
         padding: 0,
         margin: 0,
@@ -12,6 +13,7 @@ pub const fn v<T: Tuple>(mut widgets: T) -> Container<T> {
 pub const fn h<T: Tuple>(mut widgets: T) -> Container<T> {
     Container {
         widgets,
+        area: None,
         direction: Direction::Horizontal,
         padding: 0,
         margin: 0,
@@ -27,30 +29,47 @@ pub enum Direction {
 pub struct Container<T: Tuple> {
     pub widgets: T,
     pub direction: Direction,
-    //TODO: Does the user really need to know the area?
-    // pub area: Rect,
+    pub area: Option<Rect>,
     ///Outer padding
     pub padding: usize,
     ///Inner padding
     pub margin: usize,
 }
 
+//TODO: Should the inital position be based on the first widget.
+//Or should the user define that themselves.
 impl<T: Tuple> Container<T> {
     pub fn calculate(&mut self) {
         let mut y: i32 = -1;
         let mut x: i32 = -1;
+
         let padding = self.padding as i32;
         let margin = self.margin as i32;
         let direction = self.direction;
+
+        let mut root_area = Rect::new(0, 0, 0, 0);
+        let mut max_width = 0;
+        let mut max_height = 0;
 
         self.widgets.for_each(&mut |f| {
             let area = f.area();
             let height = area.height;
             let width = area.width;
 
+            if width > max_width {
+                max_width = width;
+            }
+
+            if height > max_height {
+                max_height = height;
+            }
+
             if y < 0 || x < 0 {
                 x = area.x;
                 y = area.y;
+
+                root_area.x = x;
+                root_area.y = y;
             } else {
                 area.x = x;
                 area.y = y;
@@ -61,10 +80,33 @@ impl<T: Tuple> Container<T> {
             }
 
             match direction {
-                Direction::Vertical => y += height + padding,
-                Direction::Horizontal => x += width + padding,
+                Direction::Vertical => {
+                    root_area.height += height + padding;
+                    y += height + padding;
+                }
+                Direction::Horizontal => {
+                    root_area.width += width + padding;
+                    x += width + padding;
+                }
             }
         });
+
+        match direction {
+            Direction::Vertical => {
+                root_area.width = max_width;
+                root_area.height -= padding;
+            }
+            Direction::Horizontal => {
+                root_area.height = max_height;
+                root_area.width -= padding;
+            }
+        }
+
+        self.area = Some(root_area);
+    }
+    pub fn area(&mut self) -> Rect {
+        self.calculate();
+        self.area.as_ref().unwrap().clone()
     }
     pub fn padding(mut self, padding: usize) -> Self {
         self.padding = padding;
