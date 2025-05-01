00ddb529c4cc13a7635b055fbc793556885d0add
zX3no <28522740+zX3no@users.noreply.github.com>
2025-05-01 13:49:09 +0930

    cleanup

--- examples/test.rs
+++ examples/test.rs
@@ -3,11 +3,6 @@ use softui::*;
 fn main() {
     let ctx = create_ctx("Softui", 800, 600);
 
-    //Why does this need to be drawn three times.
-    ctx.draw_frame();
-    ctx.draw_frame();
-    ctx.draw_frame();
-
     loop {
         match ctx.event() {
             Some(Event::Quit | Event::Input(Key::Escape, _)) => break,
@@ -25,6 +20,6 @@ fn main() {
             .gap(20));
         }
 
-        // ctx.draw_frame();
+        ctx.draw_frame();
     }
 }
