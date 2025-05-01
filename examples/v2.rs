6966f6e892047788e8e12d8ecb82fd0beb47e5c7
zX3no <28522740+zX3no@users.noreply.github.com>
2025-05-01 14:45:31 +0930

    behaviours

--- examples/v2.rs
+++ examples/v2.rs
@@ -1,25 +1,48 @@
 use softui::*;
 
-pub trait Position: Sized + AnyWidget {
+pub trait WidgetNew: Sized + AnyWidget {
     fn x(mut self, x: usize) -> Self {
         let area = self.area_mut();
         area.x = x;
         self
     }
+
+    fn on_click(mut self, button: MouseButton, function: fn(&mut dyn AnyWidget)) -> Self {
+        if let Some(behaviour) = self.behaviour_mut() {
+            behaviour.push(AnyClick {
+                button,
+                action: MouseAction::Clicked,
+                function,
+            });
+        }
+        self
+    }
+}
+
+pub struct AnyClick {
+    pub button: MouseButton,
+    pub action: MouseAction,
+    pub function: fn(&mut dyn AnyWidget),
 }
 
 pub trait AnyWidget {
     fn area(&self) -> Rect;
     fn area_mut(&mut self) -> &mut Rect;
-    fn behaviour(&self) {}
     fn primative(&self) {}
+    fn behaviour(&mut self) -> Option<&[AnyClick]> {
+        None
+    }
+    fn behaviour_mut(&mut self) -> Option<&mut Vec<AnyClick>> {
+        None
+    }
 }
 
 struct V {
     area: Rect,
+    behaviour: Vec<AnyClick>,
 }
 
-impl Position for V {}
+impl WidgetNew for V {}
 
 impl AnyWidget for V {
     fn area(&self) -> Rect {
@@ -28,10 +51,32 @@ impl AnyWidget for V {
     fn area_mut(&mut self) -> &mut Rect {
         &mut self.area
     }
+    fn behaviour(&mut self) -> Option<&[AnyClick]> {
+        Some(&self.behaviour)
+    }
+    fn behaviour_mut(&mut self) -> Option<&mut Vec<AnyClick>> {
+        Some(&mut self.behaviour)
+    }
+}
+
+fn widgets(widgets: &mut [&mut dyn AnyWidget]) {
+    for widget in widgets {
+        if let Some(behaviours) = widget.behaviour() {
+            for b in behaviours {
+                // (b.function)(*widget);
+            }
+        }
+    }
 }
 
 fn main() {
-    let widget = V { area: Rect::default() }.x(10);
-    let dyn_widget = &widget as &dyn AnyWidget;
-    dbg!(dyn_widget.area());
+    let mut widget = V {
+        area: Rect::default(),
+        behaviour: Vec::new(),
+    }
+    .x(10)
+    .on_click(Left, |s| println!("Clicked left mouse."));
+
+    let dyn_widget = &mut widget as &mut dyn AnyWidget;
+    widgets(&mut [dyn_widget]);
 }
