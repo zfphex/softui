b051ae6ca193cebad81856774f2cd2f5269dad1f
zX3no <28522740+zX3no@users.noreply.github.com>
2025-05-01 14:06:56 +0930

    cleanup dyn widget example

--- examples/v2.rs
+++ examples/v2.rs
@@ -1,34 +1,37 @@
 use softui::*;
 
 pub trait Position: Sized + AnyWidget {
-    fn x(self, x: usize) -> Self;
+    fn x(mut self, x: usize) -> Self {
+        let area = self.area_mut();
+        area.x = x;
+        self
+    }
 }
 
 pub trait AnyWidget {
     fn area(&self) -> Rect;
+    fn area_mut(&mut self) -> &mut Rect;
     fn behaviour(&self) {}
     fn primative(&self) {}
 }
 
 struct V {
-    x: usize,
+    area: Rect,
 }
 
-impl Position for V {
-    fn x(mut self, x: usize) -> Self {
-        self.x = x;
-        self
-    }
-}
+impl Position for V {}
 
 impl AnyWidget for V {
     fn area(&self) -> Rect {
-        Rect::new(self.x, 0, 0, 0)
+        self.area
+    }
+    fn area_mut(&mut self) -> &mut Rect {
+        &mut self.area
     }
 }
 
 fn main() {
-    let widget = V { x: 10 };
+    let widget = V { area: Rect::default() }.x(10);
     let dyn_widget = &widget as &dyn AnyWidget;
     dbg!(dyn_widget.area());
 }
